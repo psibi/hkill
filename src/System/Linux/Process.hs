5ab4d4b7e51812c0a51997592f076739d4fb0335
Sibi Prabakaran <sibi@psibi.in>
2017-09-03 18:15:58 +0530

    Add Linux related code for fetching system processes

---
+++ src/System/Linux/Process.hs
@@ -0,0 +1,96 @@
+{-#LANGUAGE BangPatterns#-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings#-}
+{-# LANGUAGE ScopedTypeVariables#-}
+
+module System.Linux.Process where
+
+import Data.Monoid ((<>))
+import Data.Attoparsec.Text hiding (take)
+import Control.Applicative
+import Data.Text (Text, unpack, pack)
+import Data.Char (isDigit)
+import System.FilePath (takeFileName, (</>))
+import System.Posix.Files (isDirectory, getFileStatus)
+import System.Directory (listDirectory)
+import Control.Monad (filterM)
+
+newtype Pid = Pid { pid :: Int } deriving (Show, Read, Eq, Ord)
+
+data ProcessInfo = ProcessInfo {
+  procName :: Text,
+  procState :: Text,
+  procPid :: Pid,
+  procPpid :: Pid,
+  procVmSize :: Text,
+  procThreads :: Int
+} deriving (Show, Eq, Ord)
+
+procFs :: String
+procFs = "/proc"
+
+processInfo :: Pid -> String
+processInfo id = "/proc/" <> (show $ pid id) <> "/status"
+
+skipLines :: Text -> Parser Text
+skipLines txt = do
+  string txt <|> (takeTill isEndOfLine *> endOfLine *> skipLines txt)
+
+skipLines_ :: Text -> Parser ()
+skipLines_ txt = skipLines txt >> return ()
+
+pidParser :: Text -> Parser Pid
+pidParser txt = do
+  return $ Pid $ read $ unpack txt
+
+intParser :: Text -> Parser Int
+intParser txt = return $ read $ unpack txt
+
+parseProcessInfo :: Parser ProcessInfo
+parseProcessInfo = do
+  procName <- string "Name:" *> skipWhile (isHorizontalSpace) *> takeTill isEndOfLine
+  endOfLine
+  procState <- string "State:" *> skipWhile isHorizontalSpace *> takeTill isEndOfLine
+  endOfLine
+  skipLines_ "Pid:"
+  procPid <- skipWhile isHorizontalSpace *> takeTill isEndOfLine >>= pidParser
+  endOfLine
+  skipLines_ "PPid:"
+  procPpid <- skipWhile isHorizontalSpace *> takeTill isEndOfLine >>= pidParser       
+  endOfLine
+  skipLines_ "VmSize:"
+  procVmSize <- skipWhile isHorizontalSpace *> takeTill isEndOfLine
+  endOfLine
+  skipLines_ "Threads:"
+  procThreads <- skipWhile isHorizontalSpace *> takeTill isEndOfLine >>= intParser
+  return $ ProcessInfo { .. }
+
+getProcessInfo :: Pid -> IO String
+getProcessInfo pid = do
+  !contents <- readFile (processInfo pid)
+  return contents
+
+isNumber :: String -> Bool
+isNumber [] = False
+isNumber xs = Prelude.all isDigit xs
+
+isPidFile :: FilePath -> IO Bool
+isPidFile fp = do
+  fstatus <- getFileStatus $ ("/proc" </> fp)
+  return $ isDirectory fstatus && (isNumber fp)
+
+getPids :: IO [Pid]
+getPids = do
+  files <- listDirectory procFs
+  (files' :: [FilePath]) <- filterM isPidFile files
+  return $ map (Pid . read) files'
+
+getAllProcessInfo :: IO [String]
+getAllProcessInfo = do
+  pids <- getPids
+  mapM getProcessInfo pids
+
+getAllProcessInfoDS :: IO [Either String ProcessInfo]
+getAllProcessInfoDS = do
+  pinfos <- getAllProcessInfo
+  return $ map (\p -> parseOnly parseProcessInfo (pack p)) pinfos
