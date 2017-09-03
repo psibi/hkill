56bcdd56f825c6419527c62b415dfa447e9fd58f
Sibi Prabakaran <sibi@psibi.in>
2017-09-03 18:16:19 +0530

    Dynamically list processes now

--- app/Main.hs
+++ app/Main.hs
@@ -22,7 +22,10 @@ import qualified Data.Vector as Vec
 import qualified Graphics.Vty as V
 import Lens.Micro
 import Lens.Micro.TH
-import Lib
+import System.Linux.Process
+import Data.Text (Text)
+import qualified Data.Text as Text
+import Data.Either (isRight)
 
 searchWidget :: [String] -> Widget SearchWidget
 searchWidget xs = (str $ unlines xs)
@@ -60,29 +63,29 @@ data SearchWidget
 data KillState = KillState
   { _focusRing :: F.FocusRing SearchWidget
   , _searchEdit :: E.Editor String SearchWidget
-  , _searchResult :: List SearchWidget String
+  , _searchResult :: List SearchWidget Text
   , _logMessages :: [String]
-  , _appProcesses :: [String]
+  , _systemProcesses :: [Either String ProcessInfo]
   }
 
 makeLenses ''KillState
 
-initialState :: KillState
-initialState =
+initialState :: [Either String ProcessInfo] -> KillState
+initialState xs =
   KillState
   { _focusRing = F.focusRing [SearchEdit, SearchResult, Logger]
   , _searchEdit = E.editor SearchEdit (Just 1) ""
-  , _searchResult = killResultsView sampleProcesses
+  , _searchResult = killResultsView $ processesToNames xs
   , _logMessages = []
-  , _appProcesses = sampleProcesses
+  , _systemProcesses = xs
   }
 
-listDrawElement :: Bool -> String -> Widget SearchWidget
+listDrawElement :: Bool -> Text -> Widget SearchWidget
 listDrawElement sel a =
   let selStr s =
         if sel
-          then str $ ("* " <> s)
-          else str s
+          then txt $ ("* " <> s)
+          else txt s
   in selStr a
 
 sampleLog :: [String]
@@ -112,10 +115,7 @@ drawUI st = [ui]
           (keyBindingWidget <=> hBorder <=> logWidget (st ^. logMessages))
         ]
 
-sampleProcesses :: [String]
-sampleProcesses = ["emacs", "joe", "systemd"]
-
-killResultsView :: [String] -> List SearchWidget String
+killResultsView :: [Text] -> List SearchWidget Text
 killResultsView xs = list SearchResult (Vec.fromList xs) 1
 
 appCursor ::
@@ -123,10 +123,6 @@ appCursor ::
   -> [T.CursorLocation SearchWidget]
   -> Maybe (T.CursorLocation SearchWidget)
 appCursor = F.focusRingCursor (^. focusRing)
-
-handleLoggerViewEVent ::
-     V.Event -> Widget SearchWidget -> T.EventM SearchWidget (T.Next KillState)
-handleLoggerViewEVent = error "nopey"
 
 appEvent ::
      KillState
@@ -142,11 +138,11 @@ appEvent st (T.VtyEvent ev) =
       case F.focusGetCurrent (st ^. focusRing) of
         Just SearchEdit -> do
           T.handleEventLensed st searchEdit E.handleEditorEvent ev >>= \st' -> do
-            let searchStr :: String = fold $ E.getEditContents $ _searchEdit st'
+            let searchStr :: Text = Text.pack $ fold $ E.getEditContents $ _searchEdit st'
                 filterProcesses =
-                  if searchStr == []
-                    then sampleProcesses
-                    else filter (\x -> searchStr `isPrefixOf` x) sampleProcesses
+                  if searchStr == ""
+                    then processesToNames $ st ^. systemProcesses
+                    else filter (\x -> searchStr `Text.isPrefixOf` x) (processesToNames $ st ^. systemProcesses)
                 newSt = st' {_searchResult = killResultsView filterProcesses}
             M.continue newSt
         Just SearchResult -> do
@@ -181,5 +177,16 @@ theMap =
     , (listSelectedFocusedAttr, V.black `on` V.yellow)
     ]
 
+processesToNames :: [Either String ProcessInfo] -> [Text]
+processesToNames procs = filter (\x -> x /= "") $ map (either (const "") procName) procs
+
 main :: IO ()
-main = defaultMain killApp initialState >> return ()
+main = do
+  procs <- getAllProcessInfoDS
+  defaultMain killApp $ initialState procs
+  return ()
+
+-- main :: IO ()
+-- main = do
+--   x <- test
+--   print x
